3106314890cbacd30443034a8c94c3e9d3cfd64c
yoshino <j1640@matsue-ct.jp>
2019-10-03 17:13:37 +0900

    1 commit

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+for i in 1..100 do
+ if i % 3 == 0 && i % 5 == 0 then
+   print("FizzBuzz\n")
+elsif i % 3 == 0 then
+ print("Fizz\n")
+elsif i%5==0 then
+ print("Buzz\n")
+else
+print(i,"\n")
+end
+
+end
+
